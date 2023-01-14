c9ea0e36121df1c519362fc09f98178189328031
AZAG Djessim <djessim-externe.azag@enedis.fr>
2023-01-14 11:42:51 +0100

    chore: initialisation du projet

---
+++ observation-animaux-marins-api/src/main/java/nc.observation.animaux.marins/ObservationAnimauxMarinsApplication.java
@@ -0,0 +1,12 @@
+package nc.observation.animaux.marins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservationAnimauxMarinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservationAnimauxMarinsApplication.class, args);
+	}
+}
